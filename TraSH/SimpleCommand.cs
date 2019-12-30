d44f1f60426ca6bbee60c6c560d48b62541c36e8
Vishnu Vijayan <vvijayan@purdue.edu>
2019-12-30 15:51:44 +0530

    Create simple command object and unit tests

---
+++ TraSH/SimpleCommand.cs
@@ -0,0 +1,39 @@
+﻿namespace TraSH
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class SimpleCommand
+    {
+        public SimpleCommand()
+            : this(string.Empty, new List<string> ())
+        {
+        }
+
+        public SimpleCommand(string command, IEnumerable<string> arguments)
+        {
+            this.Command = command;
+            this.Arguments = arguments.ToList();
+        }
+
+        public string Command { get; set; }
+
+        public List<string> Arguments { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(this.Command);
+
+            if (this.Arguments.Count > 0)
+            {
+                sb.Append(" ");
+                sb.Append(string.Join(" ", this.Arguments));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TraSHTest/SimpleCommandTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TraSH;
+
+namespace TraSHTest
+{
+    [TestClass]
+    public class SimpleCommandTest
+    {
+        [TestMethod]
+        public void InitializeEmptyTest()
+        {
+            var sc = new SimpleCommand();
+
+            sc.Command.Should().BeNullOrEmpty();
+            sc.Arguments.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void InitializeCommandAndArgsTest()
+        {
+            var sc = new SimpleCommand(
+                "git",
+                new List<string> { "reset", "--hard" });
+
+            sc.Command.Should().Be("git");
+            sc.Arguments.Should().BeEquivalentTo(new List<string> { "reset", "--hard" }, opt => opt.WithStrictOrdering());
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            var sc = new SimpleCommand(
+                "git",
+                new List<string> { "reset", "--hard" });
+            string expectedString = "git reset --hard";
+
+            string actualString = sc.ToString();
+
+            actualString.Should().Be(expectedString);
+        }
+    }
+}
