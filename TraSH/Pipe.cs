dd635d31a37224e1982a0822b384a8d64ebf9adf
Vishnu Vijayan <vvijayan@purdue.edu>
2020-01-06 13:49:55 +0530

    Implement basic pipe and write unit tests

---
+++ TraSH/Pipe.cs
@@ -0,0 +1,116 @@
+﻿namespace TraSH
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Pipe
+    {
+        private readonly Process inProc;
+        private readonly Process outProc;
+        private readonly CancellationTokenSource tokenSource;
+
+        public Pipe(Process inProc, Process outProc)
+        {
+            this.inProc = inProc;
+            this.outProc = outProc;
+            this.IsClosed = true;
+            this.tokenSource = new CancellationTokenSource();
+        }
+
+        public Pipe(StreamReader inStreamReader, Process outProc)
+        {
+            this.InStreamReader = inStreamReader;
+            this.outProc = outProc;
+            this.IsClosed = true;
+            this.tokenSource = new CancellationTokenSource();
+        }
+
+        public Pipe(Process inProc, StreamWriter outStreamWriter)
+        {
+            this.inProc = inProc;
+            this.OutStreamWriter = outStreamWriter;
+            this.IsClosed = true;
+            this.tokenSource = new CancellationTokenSource();
+        }
+
+        public Pipe(StreamReader inStreamReader, TextWriter outStreamWriter)
+        {
+            this.OutStreamWriter = outStreamWriter;
+            this.InStreamReader = inStreamReader;
+            this.IsClosed = true;
+            this.tokenSource = new CancellationTokenSource();
+        }
+
+        public void Start()
+        {
+            if (this.outProc != null)
+            {
+                this.OutStreamWriter = this.outProc.StandardInput;
+            }
+
+            if (this.inProc != null)
+            {
+                this.InStreamReader = this.inProc.StandardOutput;
+            }
+
+            this.IsClosed = false;
+            this.PipeTask = Task.Run(() => this.PipeOutput(this.tokenSource.Token));
+        }
+
+        public void Close()
+        {
+            this.IsClosed = true;
+            this.tokenSource.Cancel();
+        }
+
+        public void Wait()
+        {
+            if (this.PipeTask == null)
+            {
+                return;
+            }
+
+            this.PipeTask.Wait();
+        }
+
+        public StreamReader InStreamReader { get; private set; }
+
+        public TextWriter OutStreamWriter { get; private set; }
+
+        public bool IsClosed { get; private set; }
+
+        private Task PipeTask { get; set; }
+
+        private void PipeOutput(CancellationToken token)
+        {
+            while (true)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    string content = this.InStreamReader.ReadToEnd();
+                    this.OutStreamWriter.Write(content);
+                    break;
+                }
+                else if (!this.InStreamReader.EndOfStream)
+                {
+                    char content = (char)this.InStreamReader.Read();
+                    this.OutStreamWriter.Write(content);
+
+                    if (content == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            this.InStreamReader.Close();
+            this.OutStreamWriter.Close();
+            this.IsClosed = true;
+        }
+    }
+}
---
+++ TraSHTest/PipeTest.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraSH;
+
+namespace TraSHTest
+{
+    [TestClass]
+    public class PipeTest
+    {
+        [TestMethod]
+        public void SinglePipeTest()
+        {
+            StreamReader inStream = StreamFromText("Some pipeline input text");
+            StringWriter outStream = OutputStream(out StringBuilder sb);
+            Pipe pipe = new Pipe(inStream, outStream);
+
+            pipe.Start();
+            pipe.Close();
+            pipe.Wait();
+
+            sb.ToString().Should().Be("Some pipeline input text");
+            pipe.IsClosed.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void MultiplePipesTest()
+        {
+            StreamReader inStream1 = StreamFromText("Some pipeline input text");
+            StringWriter outStream1 = OutputStream(out StringBuilder sb1);
+            Pipe pipe1 = new Pipe(inStream1, outStream1);
+            pipe1.Start();
+            pipe1.Close();
+            pipe1.Wait();
+
+            StreamReader inStream2 = StreamFromText(sb1.ToString().ToUpper());
+            StringWriter outStream2 = OutputStream(out StringBuilder sb2);
+            Pipe pipe2 = new Pipe(inStream2, outStream2);
+            pipe2.Start();
+            pipe2.Close();
+            pipe2.Wait();
+
+            StreamReader inStream3 = StreamFromText(string.Join("", sb2.ToString().Split(" ")));
+            StringWriter outStream3 = OutputStream(out StringBuilder sb3);
+            Pipe pipe3 = new Pipe(inStream3, outStream3);
+            pipe3.Start();
+            pipe3.Close();
+            pipe3.Wait();
+
+            sb3.ToString().Should().Be("SOMEPIPELINEINPUTTEXT");
+            pipe1.IsClosed.Should().BeTrue();
+            pipe2.IsClosed.Should().BeTrue();
+            pipe3.IsClosed.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void EmptyPipeTest()
+        {
+            StreamReader inStream = StreamFromText("");
+            StringWriter outStream = OutputStream(out StringBuilder sb);
+            Pipe pipe = new Pipe(inStream, outStream);
+
+            pipe.Start();
+            pipe.Close();
+            pipe.Wait();
+
+            sb.ToString().Should().Be("");
+            pipe.IsClosed.Should().BeTrue();
+        }
+
+        private static StreamReader StreamFromText(string s)
+        {
+            byte[] byteArray = Encoding.UTF8.GetBytes(s);
+            Stream stream = new MemoryStream(byteArray);
+            StreamReader reader = new StreamReader(stream);
+
+            return reader;
+        }
+
+        private static StringWriter OutputStream(out StringBuilder sb)
+        {
+            sb = new StringBuilder();
+            StringWriter outStream = new StringWriter(sb);
+            return outStream;
+        }
+    }
+}
